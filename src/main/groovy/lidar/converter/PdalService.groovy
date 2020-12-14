a7b8d7e5ab072d45c03163c4e983fc9706dc36c2
sbortman <scott.bortman@gmail.com>
2020-12-14 12:12:02 -0500

    used pdal info to check if Red, Green, and Blue channels are present.   If so,  use RGB for meaterial in potree converter.   If they are not,  fall back and use Intensity.

--- src/main/groovy/lidar/converter/PdalService.groovy
+++ src/main/groovy/lidar/converter/PdalService.groovy
@@ -7,13 +7,40 @@ import geoscript.proj.Projection
 
 @Singleton
 class PdalService {
-    String getBboxWkt(File inputFile) {
-        def metadata = "pdal info --metadata ${inputFile}".execute().text
-        def json = new JsonSlurper().parseText(metadata)
-        def proj = new Projection(json?.metadata?.spatialreference)
-        def coords = ['minx', 'miny', 'maxx', 'maxy'].collect { json?.metadata[it] }
-        def wkt = new Bounds(*coords, proj).reproject('epsg:4326')?.polygon?.wkt
+  String getBboxWkt( File inputFile ) {
+    def metadata = "pdal info --metadata ${ inputFile }".execute().text
+    def json = new JsonSlurper().parseText( metadata )
+    def proj = new Projection( json?.metadata?.spatialreference )
+    def coords = [ 'minx', 'miny', 'maxx', 'maxy' ].collect { json?.metadata[ it ] }
+    def wkt = new Bounds( *coords, proj ).reproject( 'epsg:4326' )?.polygon?.wkt
 
-        return wkt
+    return wkt
+  }
+
+  String getMaterial( File lidarFile ) {
+    def cmd = [
+        'pdal', 'info',
+        lidarFile
+    ]
+
+    def p = cmd.execute()
+    def stdout = new StringBuilder()
+    def stderr = new StringBuilder()
+
+    p.consumeProcessOutput( stdout, stderr )
+
+    if ( !p.waitFor() ) {
+      //println stdout
+      def json = new JsonSlurper().parseText( stdout.toString() )
+      def channels = json?.stats?.statistic*.name
+
+      if ( channels.containsAll( [ 'Red', 'Green', 'Blue' ] ) ) {
+        return 'RGB'
+      } else {
+        return 'ELEVATION'
+      }
+    } else {
+      System.err.println( stderr )
     }
+  }
 }
